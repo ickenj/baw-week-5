2a3264a627c9571e3b5c32f9b718786eaaf98043
Joanne Icken <ickenj@gmail.com>
2012-10-10 12:45:54 -0400

    added exercise files

---
+++ Exercise3.php
@@ -0,0 +1,21 @@
+<?php
+/*******************************************************************
+ * Author:   Joanne Icken
+ * Purpose:  This program calculates the discount for a price that's
+ *           entered
+ */
+
+$price = 9.50;
+// $price = 10.40;
+// Calculate the discount
+$discount10 = '10';
+$discount20 = '20';
+if ($price <=10.00) {
+	$discount_amount = ($price / 100) * $discount10;
+	$final_price = $price - $discount_amount;
+	echo "Your price is $".$final_price. " with a 10% discount\n";
+} else {
+	$discount_amount = ($price / 100) * $discount20;
+	$final_price = $price - $discount_amount;
+	echo "Your price is $".$final_price. " with a 20% discount\n";
+}
---
+++ Exercise4.php
@@ -0,0 +1,15 @@
+<?php
+/*******************************************************************
+* Author:   Joanne Icken
+* Purpose:  This program prints a times table
+*/
+
+$times = 5;
+$start = 1;
+$end = 10;
+
+echo "Here is the 5's table\n";
+for($start;$start<=$end;$start++) {
+	$answer=$times * $start;
+	echo $times."x".$start."=".$answer."\n";
+}
